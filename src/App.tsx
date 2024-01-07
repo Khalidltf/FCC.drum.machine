fe3f656da07eb758fd30fbe1501a50f35b6e2c76
khalidlotfi <khalidlotfi2005@gmail.com>
2024-01-07 12:03:52 +0100

    deleting some comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,55 +4,46 @@ import Drum from "./Drum";
 
 const audioClips: AudioClip[] = [
   {
-    // keyCode: 81,
     keyTrigger: "Q",
     description: "Heater-1",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
   },
   {
-    // keyCode: 87,
     keyTrigger: "W",
     description: "Heater-2",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
   },
   {
-    // keyCode: 69,
     keyTrigger: "E",
     description: "Heater-3",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
   },
   {
-    // keyCode: 65,
     keyTrigger: "A",
     description: "Heater-4",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
   },
   {
-    // keyCode: 83,
     keyTrigger: "S",
     description: "Clap",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
   },
   {
-    // keyCode: 68,
     keyTrigger: "D",
     description: "Open-HH",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
   },
   {
-    // keyCode: 90,
     keyTrigger: "Z",
     description: "Kick-n'-Hat",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
   },
   {
-    // keyCode: 88,
     keyTrigger: "X",
     description: "Kick",
     url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
   },
   {
-    // keyCode: 67,
     keyTrigger: "C",
     description: "Closed-HH",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
